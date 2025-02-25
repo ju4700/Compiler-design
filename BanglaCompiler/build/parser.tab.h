b1066e04d2030bdf61938e0279f46e63015206a2
ju4700 <jalal.dev.design@gmail.com>
2025-02-25 19:36:44 +0600

    Add generated Bison parser header and CMake cache file for BanglaCompiler

---
+++ BanglaCompiler/build/parser.tab.h
@@ -0,0 +1,119 @@
+/* A Bison parser, made by GNU Bison 3.8.2.  */
+
+/* Bison interface for Yacc-like parsers in C
+
+   Copyright (C) 1984, 1989-1990, 2000-2015, 2018-2021 Free Software Foundation,
+   Inc.
+
+   This program is free software: you can redistribute it and/or modify
+   it under the terms of the GNU General Public License as published by
+   the Free Software Foundation, either version 3 of the License, or
+   (at your option) any later version.
+
+   This program is distributed in the hope that it will be useful,
+   but WITHOUT ANY WARRANTY; without even the implied warranty of
+   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+   GNU General Public License for more details.
+
+   You should have received a copy of the GNU General Public License
+   along with this program.  If not, see <https://www.gnu.org/licenses/>.  */
+
+/* As a special exception, you may create a larger work that contains
+   part or all of the Bison parser skeleton and distribute that work
+   under terms of your choice, so long as that work isn't itself a
+   parser generator using the skeleton or a modified version thereof
+   as a parser skeleton.  Alternatively, if you modify or redistribute
+   the parser skeleton itself, you may (at your option) remove this
+   special exception, which will cause the skeleton and the resulting
+   Bison output files to be licensed under the GNU General Public
+   License without this special exception.
+
+   This special exception was added by the Free Software Foundation in
+   version 2.2 of Bison.  */
+
+/* DO NOT RELY ON FEATURES THAT ARE NOT DOCUMENTED in the manual,
+   especially those whose name start with YY_ or yy_.  They are
+   private implementation details that can be changed or removed.  */
+
+#ifndef YY_YY_HOME_J47_DOCUMENTS_COMPILER_COMPILER_DESIGN_BANGLACOMPILER_BUILD_PARSER_TAB_H_INCLUDED
+# define YY_YY_HOME_J47_DOCUMENTS_COMPILER_COMPILER_DESIGN_BANGLACOMPILER_BUILD_PARSER_TAB_H_INCLUDED
+/* Debug traces.  */
+#ifndef YYDEBUG
+# define YYDEBUG 0
+#endif
+#if YYDEBUG
+extern int yydebug;
+#endif
+
+/* Token kinds.  */
+#ifndef YYTOKENTYPE
+# define YYTOKENTYPE
+  enum yytokentype
+  {
+    YYEMPTY = -2,
+    YYEOF = 0,                     /* "end of file"  */
+    YYerror = 256,                 /* error  */
+    YYUNDEF = 257,                 /* "invalid token"  */
+    NUMBER = 258,                  /* NUMBER  */
+    FLOAT = 259,                   /* FLOAT  */
+    STRING = 260,                  /* STRING  */
+    BOOL = 261,                    /* BOOL  */
+    IF = 262,                      /* IF  */
+    ELSE = 263,                    /* ELSE  */
+    WHILE = 264,                   /* WHILE  */
+    FUNCTION = 265,                /* FUNCTION  */
+    RETURN = 266,                  /* RETURN  */
+    PRINT = 267,                   /* PRINT  */
+    ASSIGN = 268,                  /* ASSIGN  */
+    SEMICOLON = 269,               /* SEMICOLON  */
+    LPAREN = 270,                  /* LPAREN  */
+    RPAREN = 271,                  /* RPAREN  */
+    LBRACE = 272,                  /* LBRACE  */
+    RBRACE = 273,                  /* RBRACE  */
+    GT = 274,                      /* GT  */
+    LT = 275,                      /* LT  */
+    EQ = 276,                      /* EQ  */
+    PLUS = 277,                    /* PLUS  */
+    MINUS = 278,                   /* MINUS  */
+    MUL = 279,                     /* MUL  */
+    DIV = 280,                     /* DIV  */
+    AND = 281,                     /* AND  */
+    OR = 282,                      /* OR  */
+    INTEGER = 283,                 /* INTEGER  */
+    FLOATVAL = 284,                /* FLOATVAL  */
+    TRUE = 285,                    /* TRUE  */
+    FALSE = 286,                   /* FALSE  */
+    STRINGVAL = 287,               /* STRINGVAL  */
+    IDENTIFIER = 288               /* IDENTIFIER  */
+  };
+  typedef enum yytokentype yytoken_kind_t;
+#endif
+
+/* Value type.  */
+#if ! defined YYSTYPE && ! defined YYSTYPE_IS_DECLARED
+union YYSTYPE
+{
+#line 9 "src/parser.y"
+
+    int ival;
+    float fval;
+    bool bval;
+    char* sval;
+    ASTNode* node;
+
+#line 105 "/home/j47/Documents/Compiler/Compiler-design/BanglaCompiler/build/parser.tab.h"
+
+};
+typedef union YYSTYPE YYSTYPE;
+# define YYSTYPE_IS_TRIVIAL 1
+# define YYSTYPE_IS_DECLARED 1
+#endif
+
+
+extern YYSTYPE yylval;
+
+
+int yyparse (void);
+
+
+#endif /* !YY_YY_HOME_J47_DOCUMENTS_COMPILER_COMPILER_DESIGN_BANGLACOMPILER_BUILD_PARSER_TAB_H_INCLUDED  */
